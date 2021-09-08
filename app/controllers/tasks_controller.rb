5930ea449420f7a3e82453f315bafa94ffb2c918
Pei Ern Koh <peiern.28@gmail.com>
2021-09-08 16:39:35 +0800

    added destroy, edit and update

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,13 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
 
-  def find
-    @tasks = Task.find(params[:id])
-    if @tasks.completed? == true
+  def show
+
+    if @task.completed? == true
       @true = 'This task is completed'
     else
       @false = 'This task is not completed'
@@ -19,11 +21,29 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     @task.save
+    redirect_to tasks_path
+  end
+
+  def edit
+  end
+
+  def update
+    @task.update(task_params)
+    redirect_to tasks_path
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
   end
 
   private
 
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
   def task_params
-    params.require(:task).permit(:title, :details, :completed)
+    params.require(:task).permit(:title, :details)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'tasks', to: 'tasks#index'
-  get 'tasks/new', to: 'tasks#new', as: :new
-  get 'tasks/:id', to: 'tasks#find', as: :task
-  get 'tasks', to: 'tasks#create', as: :create
+  # get 'tasks', to: 'tasks#index'
+  # get 'tasks/new', to: 'tasks#new', as: :new_task
+  # get 'tasks/:id', to: 'tasks#show', as: :task
+  # post 'tasks', to: 'tasks#create', as: :create
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # patch 'tasks/:id', to: 'tasks#update', as: :update
+  # delete 'tasks/:id', to: 'tasks#destory', as: :destory
+
+  resources :tasks
 end
