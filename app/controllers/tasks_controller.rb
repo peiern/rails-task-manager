6a569fecde8911887f07980a0f9f6beae3c496b3
Pei Ern Koh <peiern.28@gmail.com>
2021-09-08 13:05:12 +0800

    added new & create

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,5 +5,25 @@ class TasksController < ApplicationController
 
   def find
     @tasks = Task.find(params[:id])
+    if @tasks.completed? == true
+      @true = 'This task is completed'
+    else
+      @false = 'This task is not completed'
+    end
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
